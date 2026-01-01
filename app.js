c18c76a386031a977c581d060677badbc5119f7f
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-01-02 05:33:09 +0900

    feat:FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -31,12 +31,15 @@ app.post("/create-item", (req, res) => {
   console.log("user entered /create-item");
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("successfully added");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
+
+    // if (err) {
+    //   console.log(err);
+    //   res.end("something went wrong");
+    // } else {
+    //   res.end("successfully added");
+    // }
   });
 });
 
