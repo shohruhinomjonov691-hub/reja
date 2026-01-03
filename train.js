9563400025e4649d38fdf4b4a95f36c22441927d
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-01-03 14:53:02 +0900

    feat: MITTASK C-TASK

--- train.js
+++ train.js
@@ -1,7 +1,88 @@
+// MITTASK
+
+/* C-TASK
+* Savol: Shunday class tuzing tuzing nomi Shop,
+va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning
+3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul.
+Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop-qoldiq()
+return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud!
+shop. sotish( 'non', 3) & shop. qabul("cola', 4) & shop. qoldiq()
+return hozir 20:50da lta non, 5ta lagmon va 6ta cola mavjud!
+*/
+/*Masalaning yechimi:
+ */
+// 1 -case:
+const moment = require("moment");
+
+const time = moment().format("HH: mm");
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+
+  sotish(s, b) {
+    if (s !== "non" && s !== "cola" && s !== "lagmon") {
+      console.log(`Bizda "${s}" mahsuloti mavjud emas !`);
+    } else if (b > this.non && b > this.lagmon && b > this.cola) {
+      console.log(`${b} son noto‘g‘ri / yetarli emas`);
+    } else if (s === "non" && typeof b === "number" && b < this.non) {
+      this.non -= b;
+      console.log(`Hozirda ${time}da ${b}ta non sotildi!`);
+    } else if (s === "lagmon" && typeof b === "number" && b < this.lagmon) {
+      this.lagmon -= b;
+      console.log(`Hozirda ${time}da ${b}ta lagmon sotildi!`);
+    } else if (s === "cola" && typeof b === "number" && b < this.cola) {
+      this.cola -= b;
+      console.log(`Hozirda ${time}da ${b}ta cola sotildi!`);
+    }
+  }
+
+  qabul(q, c) {
+    if (q !== "non" && q !== "cola" && q !== "lagmon") {
+      console.log(`Bizda "${q}" mahsuloti mavjud emas !`);
+    } else if (typeof c !== "number" || c <= 0) {
+      console.log(`${c} son noto‘g‘ri / yetarli emas`);
+    } else if (q === "non" && typeof c === "number" && c >= 0) {
+      this.non += c;
+      console.log(`Hozirda ${time}da ${c}ta non qabul qilindi!`);
+    } else if (q === "lagmon" && typeof c === "number" && c >= 0) {
+      this.lagmon += c;
+      console.log(`Hozirda ${time}da ${c}ta lagmon qabul qilindi!`);
+    } else if (q === "cola" && typeof c === "number" && c >= 0) {
+      this.cola += c;
+      console.log(`Hozirda ${time}da ${c}ta cola qabul qilindi!`);
+    }
+  }
+
+  qoldiq(a) {
+    console.log(
+      `Hozir ${time}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`
+    );
+  }
+}
+
+const shop = new Shop(4, 5, 2);
+shop.qabul("non", 10);
+shop.qabul("lagmon", 10);
+shop.qabul("cola", 10);
+////=========================
+shop.sotish("non", 11);
+shop.sotish("lagmon", 11);
+shop.sotish("cola", 11);
+shop.qoldiq();
+///==========================
+shop.qabul("cola", -5);
+shop.sotish("lagmon", 10);
+shop.qoldiq();
+
 // Task-B
 // Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 //MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
-
+/*
 function countDigits(str) {
   let count = 0;
   for (let i = 0; i < str.length; i++) {
@@ -12,7 +93,7 @@ function countDigits(str) {
   return count;
 }
 console.log(countDigits("ad2a54y79we3t0sfgb9"));
-
+*/
 /* TASK A
 Harf sifatida kiritilgan birinchi parametr, 
 kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
