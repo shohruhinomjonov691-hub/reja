53888d1fcb8b502f01addd08acab665242656101
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-01-06 01:22:48 +0900

    feat: Reja Loyhamizning yakuniy bosqich logic

--- app.js
+++ app.js
@@ -49,6 +49,26 @@ app.post("/delete-item", (req, res) => {
   // res.end("done");
 });
 
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  db.collection("plans").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(data.id) },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "Hamma rejalar ochirildi" });
+    });
+  }
+});
+
 app.get("/author", (req, res) => {
   res.render("author", { user: user });
 });
--- public/browser.js
+++ public/browser.js
@@ -56,8 +56,35 @@ document.addEventListener("click", function (e) {
 
   //   Edit oper
   if (e.target.classList.contains("edit-me")) {
-    alert("siz edit tugmasini bosdingiz");
+    let userInput = prompt(
+      "O'zgartirish kiriting",
+      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+    if (userInput) {
+      axios
+        .post("/edit-item", {
+          id: e.target.getAttribute("data-id"),
+          new_input: userInput,
+        })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text"
+          ).innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan harakat qiling!");
+        });
+    }
   }
+});
+
+// Clear all
+document.getElementById("clean-all").addEventListener("click", function () {
+  axios.post("/delete-all", { delete_all: true }).then((respose) => {
+    alert(respose.data.state);
+    document.location.reload();
+  });
 });
 
 /*
