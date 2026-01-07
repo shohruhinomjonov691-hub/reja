18734567ec34047ae7613cf44d9208673af323b3
shokhrukhbek <shohruhinomjonov691@gmail.com>
2026-01-07 11:11:02 +0900

    fix: Reja

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongodb.connect(
 
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 4009;
       server.listen(PORT, function () {
         console.log(
           `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
